4927f43bd5bfb6b92bd226f2b0eb56ec87eca8b4
Thiti Tongumpun <thiti_t@mfec.co.th>
2024-05-09 09:55:02 +0700

    add: vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 import million from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({
-    auto: {
-      threshold: 0.05,
-      skip: ["useBadHook", /badVariable/g],
-    },
-  }), react()],
+  plugins: [
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true
+      }
+    })],
 })
